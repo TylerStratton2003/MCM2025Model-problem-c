4ed095d85f1ffe6494430fb67e9cd597595e52db
TylerStratton2003 <tyler.stratton@plu.edu>
2025-01-26 14:59:34 -0800

    Merged two of the csv files after a bit of research.

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import tensorflow
 
 from tensorflow.python.keras.layers import Input, Dense
+from tensorflow.python.keras.models import Sequential
 
 df = pd.read_csv("summerOly_medal_counts.csv")
 #print(df)
@@ -15,4 +16,19 @@ df2 = pd.read_csv("summerOly_hosts.csv")
 df_merged = df.merge(df2, on="Year", how='outer')
 print(df_merged)
 
+X = df_merged[['Year', 'Host', 'NOC']].values
+y = df_merged[['Gold', 'Silver', 'Bronze', 'Total']].values
+
+model = Sequential()
+model.add(Dense(8, input_dim=3, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.fit(X, y, epochs=100, batch_size=1, verbose=1)
+
+test_data = np.array([['2028', 'Los Angeles, United States', 'United States']])
+
+prediction = model.predict(test_data)
+predicted_label = (prediction > 0.5).astype(int)
+print(f"Predicted label: {predicted_label[0][0]}")
 
